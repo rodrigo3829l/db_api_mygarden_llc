5af757d1aee4bbeb8c5f8e8a2e625be692014c33
Rodrigo Del Angel Gerardo <20210658@uthh.edu.mx>
2024-02-16 06:08:56 -0600

    Correcciones de seguridad

--- helpers/utils/cloudinary.js
+++ helpers/utils/cloudinary.js
@@ -1,9 +1,9 @@
 import {v2 as cloudinary} from 'cloudinary';
           
 cloudinary.config({ 
-  cloud_name: 'dltytaqui', 
-  api_key: '556788988246927', 
-  api_secret: 'WbSLZFIkOoG3h3IaCfgJN4ZOc0A', 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key:  process.env.API_KEY, 
+  api_secret:  process.env.API_SECRET, 
   secure: true
 });
 
