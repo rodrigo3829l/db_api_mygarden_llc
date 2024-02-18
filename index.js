d459250fb090c6e63adc8b31825c0286f0cff647
Rodrigo Del Angel Gerardo <20210658@uthh.edu.mx>
2024-02-17 22:55:09 -0600

    Correcciones de seguridad

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import helmet from 'helmet';
 
 const app = express();
 console.log("Hola db => ", process.env.URI_MONGO);
+console.log("Commit de seguridad")
 
 const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2 , process.env.ORIGIN3]
 
